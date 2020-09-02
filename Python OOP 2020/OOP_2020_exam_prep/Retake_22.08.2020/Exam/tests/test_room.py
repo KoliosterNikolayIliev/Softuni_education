5ae5bb4f43df4d5756227e160d51c7ae7a1d3cc6
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2020-09-02 16:31:48 +0300

    Solved finally and tests

--- Python OOP 2020/OOP_2020_exam_prep/Retake_22.08.2020/Exam/tests/test_room.py
+++ Python OOP 2020/OOP_2020_exam_prep/Retake_22.08.2020/Exam/tests/test_room.py
@@ -1,29 +1,40 @@
-from project.appliances.laptop import Laptop
-from project.appliances.tv import TV
 from project.people.child import Child
 from project.rooms.room import Room
 import unittest
 
 
 class TestRoom(unittest.TestCase):
-
     def setUp(self):
-        self.child_one = Child(5, 1, 2, 1)
-        self.child_two = Child(3, 2)
-        self.room = Room("Smiths", 1000, 4)
+        child_one = Child(5, 1, 2, 1)
+        child_two = Child(3, 2)
+        self.room = Room("Test", 500, 2)
+        self.room.children.append(child_one)
+        self.room.children.append(child_two)
 
-    def test_set_expenses_OK(self):
-        self.room.expenses = 10
-        self.assertEqual(self.room.expenses, 10)
+    def test_set_room_init_normal(self):
+        self.assertEqual(self.room.family_name, "Test")
+        self.assertEqual(self.room.expenses, 0)
+        self.assertEqual(self.room.members_count, 2)
+        self.assertEqual(self.room.budget, 500)
+        self.assertEqual(len(self.room.children), 2)
+
+    def test_calculate_expenses_no_args(self):
+        self.room.calculate_expenses()
+        self.assertEqual(self.room.expenses, 0)
+
+    def test_calculate_expenses_args_normal(self):
+        self.room.calculate_expenses(self.room.children)
+        self.assertEqual(self.room.expenses, 420)
 
     def test_set_expenses_valueError(self):
+        child_one = Child(-5, -1, -2, -1)
+        child_two = Child(-3, -2)
+        self.room.children.clear()
+        self.room.children.append(child_one)
+        self.room.children.append(child_two)
         with self.assertRaises(ValueError) as ex:
-            self.room.expenses = -10
+            self.room.calculate_expenses(self.room.children)
             self.assertEqual(str(ex.exception), "Expenses cannot be negative")
-
-    def test_calculate_expenses(self):
-        self.room.calculate_expenses((Child(5, 1, 2, 1), Child(3, 2), TV(), Laptop()))
-        self.assertEqual(self.room.expenses, 495)
 
 
 if __name__ == "__main__":
