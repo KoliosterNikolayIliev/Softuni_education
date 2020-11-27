a96bccf6879b58df437c786b7ed33af31956604a
KoliosterNikolayIliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2020-11-27 17:51:16 +0200

    modify account.models

--- Django_Doncho/petstagram/accounts/views.py
+++ Django_Doncho/petstagram/accounts/views.py
@@ -1,13 +1,17 @@
+
+
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
 
 # Create your views here.
 from accounts.forms import SignUpForm
+from accounts.models import UserProfile
 
 
 def user_profile(request, pk):
     pass
 
-
+#yY<RggqxgY]8m7T2
 def signup_user(request):
     form = SignUpForm()
     if request.method == 'GET':
@@ -17,7 +21,10 @@ def signup_user(request):
     else:
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            profile = UserProfile(user=user,)
+            profile.save()
+            login(request, user)
             return redirect('index')
 
     context = {'form': form}
