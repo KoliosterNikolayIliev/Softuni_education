fc5de06afbb859e42a179c947a1ce444158af9a6
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-02-02 22:38:07 +0200

    ""

--- JS_ADVANCED/04.DOM_Introduction/Exercise/08. Generate Report/report.js
+++ JS_ADVANCED/04.DOM_Introduction/Exercise/08. Generate Report/report.js
@@ -1,3 +1,36 @@
 function generateReport() {
-    //TODO
+    let inputElements = Array.from(document.getElementsByTagName('input'));
+
+    const result = [];
+    let tableRows = Array.from(document.getElementsByTagName('tr'));
+    const chosenCols = [];
+
+    for (let i = 0; i < tableRows.length; i++) {
+        const row = tableRows[i];
+        const obj = {};
+
+        for (let y = 0; y < row.children.length; y++) {
+            const element = row.children[y];
+            if (i == 0) {
+                if (element.children[0].checked) {
+                    chosenCols.push(y);
+                }
+                continue;
+            }
+
+            if (chosenCols.includes(y)) {
+                let propertyName = inputElements[y].name;
+                obj[propertyName] = element.textContent;
+            }
+        }
+        if (i !== 0) {
+            if (Object.keys(obj).length > 0) {
+                result.push(obj);
+            }
+
+        }
+    }
+    if (result.length > 0) {
+        document.getElementById('output').value = JSON.stringify(result);
+    }
 }
--- JS_ADVANCED/05.DOM_Manipulations_&_events/EXERCISE/06. Furniture/testJson.js
+++ JS_ADVANCED/05.DOM_Manipulations_&_events/EXERCISE/06. Furniture/testJson.js
@@ -1,2 +1,17 @@
-let a = ' '
-console.log(a.length)
+// const sharePersonalInfo = function (...activities) {
+//     let info = `Hello, my name is ${firstPerson.name} and` + ` I'm a ${firstPerson.profession}.\n`;
+//     info += activities.reduce((acc, curr) => {let el = `--- ${curr}\n`;return acc + el;}, "My hobbies are:\n").trim();return info;};
+//
+// const firstPerson = {name: "Peter", profession: "Fisherman"};
+// console.log(sharePersonalInfo.call(firstPerson, 'biking', 'swimming', 'football'));
+
+const x = 42;
+const getX = function () {
+    return this.x;
+};
+const module = {x, getX};
+const unboundGetX = module.getX;
+console.log(unboundGetX());
+const boundGetX = unboundGetX.bind(module);
+console.log(boundGetX());
+
