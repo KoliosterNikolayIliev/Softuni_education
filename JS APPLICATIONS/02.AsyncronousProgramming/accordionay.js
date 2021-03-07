1dee4c4b0522aa387d07aaa69c8de731380601d0
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-03-07 22:35:48 +0200

    ''

--- JS APPLICATIONS/02.AsyncronousProgramming/accordionay.js
+++ JS APPLICATIONS/02.AsyncronousProgramming/accordionay.js
@@ -65,7 +65,7 @@ function e(type, attributes, ...content) {
     const result = document.createElement(type);
 
     for (let [attr, value] of Object.entries(attributes || {})) {
-        if (attr.substring(0, 2) == 'on') {
+        if (attr.substring(0, 2) === 'on') {
             result.addEventListener(attr.substring(2).toLocaleLowerCase(), value);
         } else {
             result[attr] = value;
