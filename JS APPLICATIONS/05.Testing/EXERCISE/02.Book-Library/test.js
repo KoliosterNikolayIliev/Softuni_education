5b6991648b346387fbcc00f3f5c5a08cb92d15ad
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-03-13 19:16:00 +0200

    Update test.js

--- JS APPLICATIONS/05.Testing/EXERCISE/02.Book-Library/test.js
+++ JS APPLICATIONS/05.Testing/EXERCISE/02.Book-Library/test.js
@@ -1,6 +1,7 @@
 const {firefox} = require('playwright');
-const {assert} = require('chai');
+const {assert,expect} = require('chai');
 const mockData = require('./mock-data.json');
+
 
 function jsonCustom(data) {
     return {
@@ -16,10 +17,10 @@ function jsonCustom(data) {
 let browser, page; // Declare reusable variables
 describe('E2E tests', function () {
 
-    this.timeout(6000);
+    this.timeout(20000);
     before(async () => {
 
-        browser = await firefox.launch({headless: false, slowMo: 1});
+        browser = await firefox.launch({headless: false, slowMo:1500});
     });
     after(async () => {
         await browser.close();
@@ -32,11 +33,51 @@ describe('E2E tests', function () {
     });
 
     it('load content', async () => {
-        await page.route('**/jsonstore/messenger', (request) => request.fulfill(jsonCustom(mockData)));
+        await page.route('**/jsonstore/collections/books', (request) => request.fulfill(jsonCustom(mockData)));
         await page.goto('http://localhost:3000/');
-        await page.click('text=LOAD ALL BOOKS');
-        const firstBook = page.waitFor('text=Harry Potter and the Philosopher\'s Stone')
-        assert.equal(firstBook, true);
+        await page.click('text=LOAD ALL BOOKS')
+        let content = await page.content()
+        expect(content).to.contains('Harry Potter and the Philosopher\'s Stone')
+        expect(content).to.contains('J.K.Rowling')
+        expect(content).to.contains('C# Fundamentals')
+        expect(content).to.contains('Svetlin Nakov')
+
+    });
+
+    it('adds book', async () => {
+        let book = 'Testing really sucks a lot !!!'
+        let author = 'Pesho Krivia'
+        await page.route('**/jsonstore/collections/books', (request) => request.fulfill(jsonCustom(mockData)));
+        await page.goto('http://localhost:3000/');
+        await page.fill('[name="title"]', book);
+        await page.fill('[name="author"]', author);
+        let [request] = await Promise.all([
+            page.waitForRequest(request => request.url().includes('/collections/books') && request.method() == 'POST'),
+            await page.click('text=Submit')
+        ]);
+        assert.equal(JSON.parse(request.postData()).author,author)
+        assert.equal(JSON.parse(request.postData()).title,book)
+    });
+    it.only('checks book validation', async () => {
+        await page.route('**/jsonstore/collections/books', (request) => request.fulfill(jsonCustom(mockData)));
+        await page.goto('http://localhost:3000/');
+        await page.click('text=Submit');
+        let otvrad;
+        await page.on('dialog', async dialog => {
+            otvrad = dialog.message();
+            await dialog.dismiss();
+            await browser.close();
+        });
+        page.evaluate(() => alert('1'));
+        console.log(otvrad);
+
+
+        // await page.on('dialog', async dialog => {
+        //     console.log(dialog.message());
+        //     await dialog.dismiss();
+        //     await browser.close();
+        // });
+        // page.evaluate(() => alert('1'));
 
 
     });
